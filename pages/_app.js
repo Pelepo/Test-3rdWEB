d23dcf2061b8590e8103569c4156967b66aafe78
Pelepo <andretta.filippo.99@gmail.com>
2024-03-28 10:20:57 +0100

    Testing button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
 import "@/styles/globals.css";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  embeddedWallet
+} from "./../Context/ThirdwebProvider";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const chain = "mumbai"
+
+function MyApp({ Component, pageProps }) {
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider
+        supportedWallets={[
+          metamaskWallet(),
+          coinbaseWallet(),
+          walletConnect(),
+          embeddedWallet()
+        ]}
+        activeChain={chain}
+        clientId="3d60c6caaa12c62a65cecff17b2d0a40"
+        queryClient={queryClient}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </QueryClientProvider>
+  )
 }
+
+
+export default MyApp;
